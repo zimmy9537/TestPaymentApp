4fa6acc92a37c9cfe52b8d22f55568daf471be4a
zimmy9537 <zimmy.9724@yahoo.com>
2022-02-17 00:03:03 +0530

    initial commit for now

--- app/src/main/java/android/example/testpaymentapp/MainActivity.java
+++ app/src/main/java/android/example/testpaymentapp/MainActivity.java
@@ -2,6 +2,10 @@ package android.example.testpaymentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,16 +14,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import dev.shreyaspatil.easyupipayment.EasyUpiPayment;
-import dev.shreyaspatil.easyupipayment.listener.PaymentStatusListener;
-import dev.shreyaspatil.easyupipayment.model.PaymentApp;
-import dev.shreyaspatil.easyupipayment.model.TransactionDetails;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements PaymentStatusListener {
-
-    private EditText name;
-    private EditText upiId;
     private EditText amount;
     private EditText description;
 
@@ -30,23 +29,28 @@ public class MainActivity extends AppCompatActivity implements PaymentStatusList
 
     private TextView transactionStatus;
 
-    private EasyUpiPayment easyUpiPayment;
+    String rawString="upi://pay?pa=paytmqr281005050101mm617cyacrl1@paytm&pn=Paytm%20Merchant&mc=5499&mode=02&orgid=000000&paytmqr=281005050101MM617CYACRL1&sign=MEQCID0NFi3MYLXf8Yqjqwp7AqyIM7K0nlnQNBmke8X6Ou0fAiBErCzcP25K2wUYvXyt8nJG2OOqoDEAYyVkFKVjhloZYQ==";
+    String upiId = "paytmqr281005050101mm617cyacrl1@paytm";
+    String merchantId = "JsuLrn83183937545946";
+    String accountName = "SAHGAL KUMAR";
+
+    private int REQUEST_CODE = 123;
+    private String BHIM_UPI = "in.org.npci.upiapp";
+    private String GOOGLE_PAY = "com.google.android.apps.nbu.paisa.user";
+    private String PHONE_PE = "com.phonepe.app";
+    private String PAYTM = "net.one97.paytm";
+
+    private static final int TEZ_REQUEST_CODE = 123;
+
+    private static final String GOOGLE_TEZ_PACKAGE_NAME = "com.google.android.apps.nbu.paisa.user";
 
     private String LOG_CAT = MainActivity.class.getSimpleName();
-
-    private String G_PAY = "upi://pay?pa=9872598189@okbizaxis&pn=SAHGAL  KUMAR&mc=7372&aid=uGICAgIDXtpLiAw&tr=BCR2DN6T56OLBTBY";
-
-    private String PAYTM = "upi://pay?pa=paytmqr281005050101mm617cyacrl1@paytm&pn=Paytm Merchant&mc=5499&mode=02&orgid=000000&paytmqr=281005050101MM617CYACRL1&sign=MEQCID0NFi3MYLXf8Yqjqwp7AqyIM7K0nlnQNBmke8X6Ou0fAiBErCzcP25K2wUYvXyt8nJG2OOqoDEAYyVkFKVjhloZYQ==";
-
-    private String PHONE_PE = "upi://pay?mode=02&pa=Q09575115@ybl&purpose=00&mc=0000&pn=PhonePeMerchant&orgid=180001&sign=MEUCICPlo8QO10/oVQY3jp8lF7v+f+Ud6w6fWz6THZ40SXkIAiEAx5JG1pmqeCFntaGqIopgssixTQcSkQC/SDaLEcBmLeI=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = findViewById(R.id.name);
-        upiId = findViewById(R.id.upiId);
         amount = findViewById(R.id.amount);
         description = findViewById(R.id.description);
 
@@ -57,16 +61,33 @@ public class MainActivity extends AppCompatActivity implements PaymentStatusList
 
         transactionStatus = findViewById(R.id.transaction_status);
 
+        ArrayList<String> upiApps = new ArrayList<>();
+        upiApps.add(new String(PAYTM));
+        upiApps.add(new String(GOOGLE_PAY));
+        upiApps.add(new String(PHONE_PE));
+        upiApps.add(new String(BHIM_UPI));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(rawString));
+        intent.setData(Uri.parse(upiId));
+
         googlePay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!viewsEmpty()) {
-                    transactionStatus.setVisibility(View.INVISIBLE);
-                    if (!viewsEmpty()) {
-                        googlePay();
-                    } else {
-                        Toast.makeText(MainActivity.this, "edittext empty", Toast.LENGTH_SHORT).show();
+
+                    String p=GOOGLE_TEZ_PACKAGE_NAME;
+                    if(isAppInstalled(p)&&isUpiReady(p)) {
+
+                        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(rawString));
+                        i.setData(Uri.parse(rawString));
+                        intent.setPackage(p);
+                        startActivityForResult(intent, REQUEST_CODE);
                     }
+                    else{
+                        Toast.makeText(MainActivity.this, "not here", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(MainActivity.this, "edittext empty", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -75,7 +96,6 @@ public class MainActivity extends AppCompatActivity implements PaymentStatusList
             @Override
             public void onClick(View v) {
                 if (!viewsEmpty()) {
-                    Phone_pay();
                 } else {
                     Toast.makeText(MainActivity.this, "edittext empty", Toast.LENGTH_SHORT).show();
                 }
@@ -95,7 +115,19 @@ public class MainActivity extends AppCompatActivity implements PaymentStatusList
             public void onClick(View v) {
                 transactionStatus.setVisibility(View.INVISIBLE);
                 if (!viewsEmpty()) {
-                    paytm_pay();
+
+                    String p=PAYTM;
+                    if(isAppInstalled(p)&&isUpiReady(p)) {
+
+                        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(rawString));
+                        i.setData(Uri.parse(rawString));
+                        intent.setPackage(p);
+                        startActivityForResult(intent, REQUEST_CODE);
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "not here", Toast.LENGTH_SHORT).show();
+                    }
+
                 } else {
                     Toast.makeText(MainActivity.this, "edittext empty", Toast.LENGTH_SHORT).show();
                 }
@@ -108,10 +140,33 @@ public class MainActivity extends AppCompatActivity implements PaymentStatusList
         return transactionId;
     }
 
+    boolean isAppInstalled(String packageName) {
+        PackageManager pm = getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    boolean isUpiReady(String packageName) {
+        boolean appUpiReady = false;
+        Intent upiIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("upi://pay"));
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> upiActivities= pm.queryIntentActivities(upiIntent, 0);
+        for (ResolveInfo a : upiActivities){
+            if (a.activityInfo.packageName.equals(packageName))
+            {
+                appUpiReady = true;
+            }
+        }
+        return appUpiReady;
+    }
+
     boolean viewsEmpty() {
-        if (name.getText().toString().trim().isEmpty()
-                || upiId.getText().toString().trim().isEmpty()
-                || amount.getText().toString().trim().isEmpty()
+        if (amount.getText().toString().trim().isEmpty()
                 || description.getText().toString().trim().isEmpty()) {
 
             Toast.makeText(this, "Fill all the details", Toast.LENGTH_SHORT).show();
@@ -119,157 +174,38 @@ public class MainActivity extends AppCompatActivity implements PaymentStatusList
         }
         return false;
     }
-//
-//    @SuppressWarnings("NonConstantResourceId")
-//    private void GooglePay(PaymentApp paymentApp)
-//    {
-//        //vpa is same as the upi id;
-//        String payeeVpa=upiId.getText().toString().trim();
-//        String payeeName=name.getText().toString().trim();
-//        String paymentDescription=description.getText().toString().trim();
-//        String paymentAmount=amount.getText().toString().trim();
-//        String transactionId=getTransactionId();
-//
-//        EasyUpiPayment.Builder builder=new EasyUpiPayment.Builder(this)
-//                .with(paymentApp)
-//                .setPayeeVpa(payeeVpa)
-//                .setPayeeName(payeeName)
-//                .setTransactionId();
-//    }
-
-    private void googlePay() {
-        String payeeVpa = upiId.getText().toString().trim();
-        String payeeName = name.getText().toString().trim();
-        String paymentDescription = description.getText().toString().trim();
-        String paymentAmount = amount.getText().toString().trim();
-        String transactionId = getTransactionId();
-
-        EasyUpiPayment.Builder builder = new EasyUpiPayment.Builder(this)
-                .with(PaymentApp.GOOGLE_PAY)
-                .setPayeeVpa(getGooglePayUpi())
-                .setPayeeName(payeeName)
-                .setTransactionId(transactionId)
-                .setTransactionRefId(transactionId)
-                .setPayeeMerchantCode("BCR2DN6T56OLBTBY")
-                .setDescription(paymentDescription)
-                .setAmount(paymentAmount);
-
-        try {
-            easyUpiPayment = builder.build();
-            easyUpiPayment.setPaymentStatusListener(this);
-            easyUpiPayment.startPayment();
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            Toast.makeText(this, "Error:- " + exception.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void Phone_pay() {
-        String payeeVpa = upiId.getText().toString().trim();
-        String payeeName = name.getText().toString().trim();
-        String paymentDescription = description.getText().toString().trim();
-        String paymentAmount = amount.getText().toString().trim();
-        String transactionId = getTransactionId();
-
-        EasyUpiPayment.Builder builder = new EasyUpiPayment.Builder(this)
-                .with(PaymentApp.PHONE_PE)
-                .setPayeeVpa(getphonePeUpi())
-                .setPayeeName(payeeName)
-                .setTransactionId(transactionId)
-                .setTransactionRefId(transactionId)
-                .setPayeeMerchantCode("MS2004151341148690000798")
-                .setDescription(paymentDescription)
-                .setAmount(paymentAmount);
-
-        try {
-            easyUpiPayment = builder.build();
-            easyUpiPayment.setPaymentStatusListener(this);
-            easyUpiPayment.startPayment();
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            Toast.makeText(this, "Error:- " + exception.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    private void paytm_pay() {
-        String payeeVpa = upiId.getText().toString().trim();
-        String payeeName = name.getText().toString().trim();
-        String paymentDescription = description.getText().toString().trim();
-        String paymentAmount = amount.getText().toString().trim();
-        String transactionId = getTransactionId();
-
-        EasyUpiPayment.Builder builder = new EasyUpiPayment.Builder(this)
-                .with(PaymentApp.PAYTM)
-                .setPayeeVpa(getPaytmUpi())
-                .setPayeeName(payeeName)
-                .setTransactionId(transactionId)
-                .setTransactionRefId(transactionId)
-                .setPayeeMerchantCode("paytm")
-                .setDescription(paymentDescription)
-                .setAmount(paymentAmount);
-
-        try {
-            easyUpiPayment = builder.build();
-            easyUpiPayment.setPaymentStatusListener(this);
-            easyUpiPayment.startPayment();
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            Toast.makeText(this, "Error:- " + exception.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private String getPaytmUpi() {
-        String upi = "paytmqr281005050101mm617cyacrl1@paytm";
-        return upi;
-    }
-
-    private String getGooglePayUpi() {
-        String upi = "9872598189@okbizaxis";
-        return upi;
-    }
-
-    private String getphonePeUpi() {
-        String upi = "Q09575115@ybl";
-        return upi;
-    }
 
     @Override
-    public void onTransactionCompleted(TransactionDetails transactionDetails) {
-        Toast.makeText(this, "Transaction Completed", Toast.LENGTH_SHORT).show();
-        Log.v(LOG_CAT, "Transaction details  :- " + transactionDetails);
-        switch (transactionDetails.getTransactionStatus()) {
-            case SUCCESS:
-                onTransactionSuccess();
-                break;
-            case FAILURE:
-                onTransactionFailure();
-                break;
-            case SUBMITTED:
-                onTransactionSubmitted();
-                break;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == this.REQUEST_CODE) {
+            Log.d("result", String.valueOf(data));
+            String var10000;
+            String var4;
+            boolean var5;
+            boolean var6;
+            boolean var8;
+            if (data != null) {
+                var10000 = data.getStringExtra("Status");
+                if (var10000 != null) {
+                    var4 = var10000;
+                    var5 = false;
+                    var6 = false;
+                    var8 = false;
+                    Log.d("result", var4);
+                }
+            }
+
+            if (data != null) {
+                var10000 = data.getStringExtra("Status");
+                if (var10000 != null) {
+                    var4 = var10000;
+                    var5 = false;
+                    var6 = false;
+                    var8 = false;
+                    Toast.makeText(this.getApplicationContext(), "here "+ (CharSequence)var4,Toast.LENGTH_LONG).show();
+                }
+            }
         }
-    }
-
-    private void onTransactionSubmitted() {
-        Toast.makeText(this, "Transaction Pending", Toast.LENGTH_SHORT).show();
-        transactionStatus.setText("Transaction Pending");
-    }
-
-    private void onTransactionFailure() {
-        Toast.makeText(this, "Transaction Failed", Toast.LENGTH_SHORT).show();
-        transactionStatus.setText("Transaction Failed");
-    }
-
-    private void onTransactionSuccess() {
-        Toast.makeText(this, "Transaction Successful", Toast.LENGTH_SHORT).show();
-        transactionStatus.setText("Transaction Successful");
-    }
-
-    @Override
-    public void onTransactionCancelled() {
-        Toast.makeText(this, "Transaction cancelled", Toast.LENGTH_SHORT).show();
-        transactionStatus.setVisibility(View.VISIBLE);
-        transactionStatus.setText("Transaction cancelled");
     }
 }
